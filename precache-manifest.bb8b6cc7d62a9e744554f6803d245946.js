08e44c07ce29c8592d179be31e3976ddc4e51289
ChannMyaeAung <chanmyaeaung@my.jcu.edu.au>
2022-03-22 15:41:36 +0630

    Updates

---
+++ precache-manifest.bb8b6cc7d62a9e744554f6803d245946.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab7f60a40ad67c01a69205600dff662",
+    "url": "/Moden_react_website/index.html"
+  },
+  {
+    "revision": "70f475068f7f16ac7db6",
+    "url": "/Moden_react_website/static/css/main.16d0090c.chunk.css"
+  },
+  {
+    "revision": "f93187739358926f3a5f",
+    "url": "/Moden_react_website/static/js/2.5637efa8.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/Moden_react_website/static/js/2.5637efa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e13202beaf06c0b89038",
+    "url": "/Moden_react_website/static/js/3.9b2df227.chunk.js"
+  },
+  {
+    "revision": "70f475068f7f16ac7db6",
+    "url": "/Moden_react_website/static/js/main.6d1f452a.chunk.js"
+  },
+  {
+    "revision": "2dc0940ab2d0398d089a",
+    "url": "/Moden_react_website/static/js/runtime-main.c24d9460.js"
+  },
+  {
+    "revision": "17ade55739aedbbb39e78442cdad6fb3",
+    "url": "/Moden_react_website/static/media/ai.17ade557.png"
+  },
+  {
+    "revision": "fea810ed22f6fc37fab045129a01ed07",
+    "url": "/Moden_react_website/static/media/blog01.fea810ed.png"
+  },
+  {
+    "revision": "e76b2c16b1e4ccf994a24eda75591951",
+    "url": "/Moden_react_website/static/media/blog02.e76b2c16.png"
+  },
+  {
+    "revision": "404d1a405e0cb0936f224fcd374c6cb0",
+    "url": "/Moden_react_website/static/media/blog03.404d1a40.png"
+  },
+  {
+    "revision": "d682b195d844afb2a4ce1e8573602221",
+    "url": "/Moden_react_website/static/media/blog04.d682b195.png"
+  },
+  {
+    "revision": "576efe185f7f571f39d782fde1d28fa9",
+    "url": "/Moden_react_website/static/media/blog05.576efe18.png"
+  },
+  {
+    "revision": "a95f42a40ce776c07a2891c220f620ae",
+    "url": "/Moden_react_website/static/media/logo.a95f42a4.svg"
+  },
+  {
+    "revision": "3d84899326ac5f0f66149fa037693d18",
+    "url": "/Moden_react_website/static/media/people.3d848993.png"
+  },
+  {
+    "revision": "3c84ae2642b58c3bc28340a7e763fc23",
+    "url": "/Moden_react_website/static/media/possibility.3c84ae26.png"
+  }
+]);
